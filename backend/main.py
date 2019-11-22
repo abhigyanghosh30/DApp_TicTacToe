83e54a8d1647cd6712b47c7c8341801446f7604d
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2019-11-23 03:51:01 +0530

    python contracts do infact deploy

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask, Response, request, jsonify
+from flask import Flask, Response, request, json
 from web3 import Web3
 from solc import compile_files, link_code, compile_source
 from models import db,app,Player
@@ -15,9 +15,11 @@ def deploy_contract(contract_interface):
     tx_receipt = w3.eth.getTransactionReceipt(tx_hash)
     return tx_receipt['contractAddress']
 
-contracts = compile_files(['Deployment.sol','TicTacToe.sol'])
-game_contract = contracts.pop("TicTacToe.sol:Tic")
-deployment_link = contracts.pop("Deployment.sol:DeployTic")
+contracts = compile_files(['../contracts/Deployment.sol','../contracts/TicTacToe.sol'])
+for i in contracts:
+    print(i)
+game_contract = contracts.pop("../contracts/TicTacToe.sol:Tic")
+deployment_link = contracts.pop("../contracts/Deployment.sol:DeployTic")
 
 deployment_address = {
     "Deployment.sol:DeployTic": deploy_contract(deployment_link)
@@ -44,6 +46,15 @@ def initialize():
 @app.route("/ttt/<address>")
 def add_to_database(address):
     players = Player.query.all()
-    if(len(players)%2==0):
-        pass
-    player = Player(address=address)
+    if(len(players)%2==1):
+        last=Player.query.order_by(Player.id.desc()).first()
+        gc = last.game_contract
+    else:
+        gc = deploy_contract(game_contract)
+    player = Player(address=address,game_contract=gc)
+    db.session.add(player)
+    db.session.commit()
+    return Response(json.dumps({'address':gc}), status = 200, mimetype = 'application/json')
+
+if __name__ == "__main__":
+    app.run(port=8000,debug=True)
--- backend/models.py
+++ backend/models.py
@@ -1,12 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+import os
+
 app = Flask(__name__)
 app.secret_key = os.urandom(67)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ganes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///games.db'
 db = SQLAlchemy(app)
 
 class Player(db.Model):
     __tablename__ = 'players'
-    
-	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	address = db.Column(db.String(100), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    address = db.Column(db.String(100), nullable=False)
     game_contract = db.Column(db.String(100), nullable=False)
